4325bed493854b2c7fe42e7563ae32d92ce6642a
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-04 20:07:24 +0200

    ..

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+/**
+ * binary_tree_height_2 - Description of the function
+ * @tree: Description of the 'tree' parameter
+ *
+ * Return: Description of the return value
+ */
+size_t binary_tree_height_2(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	return (max(l + 1, r + 1));
+}
+
+/**
+ * binary_tree_is_perfect - Description of the function
+ * @tree: Description of the 'tree' parameter
+ *
+ * Return: Description of the return value
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int h, hl, hr;
+	if (!tree)
+		return (0);
+	h = binary_tree_height_2(tree);
+	hl = tree->left ? binary_tree_height_2(tree->left) + 1 : 0;
+	hr = tree->right ? binary_tree_height_2(tree->right) + 1 : 0;
+
+	return ((hl == hr) && (hl == h));
+}
