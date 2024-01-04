97a81530daffb03f13e3c04f2ebaa998387f83bd
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-04 17:32:12 +0200

    ..

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Description of the function
+ * @tree: Description of the 'tree' parameter
+ *
+ * Return: Description of the return value
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (0);
+}
