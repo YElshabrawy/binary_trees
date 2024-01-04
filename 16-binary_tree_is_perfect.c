07b51e1fc5575462e821ebae4b49d4ad9482c27c
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-04 20:09:21 +0200

    ..

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,8 +14,8 @@ size_t binary_tree_height_2(const binary_tree_t *tree)
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
+	l = binary_tree_height_2(tree->left);
+	r = binary_tree_height_2(tree->right);
 	return (max(l + 1, r + 1));
 }
 
